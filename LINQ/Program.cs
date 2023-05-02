c13e3d875fdec2ca17609de8cb5cdfc97d7e5839
Matt Fuller <mnfuller552@gmail.com>
2023-05-02 14:28:24 -0400

    finished

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿using System.Linq;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> Videogames = new List<string>() { "Super Mario Bros", "Zelda" };
+            Videogames.Add("Call of Duty");
+            Videogames.Add("Grand Theft Auto");
+
+            var order = Videogames.OrderBy(name => name.Length);
+
+            foreach (var videogame in order)
+            {
+                Console.WriteLine(videogame);
+            }
         }
     }
 }
